3445149ceec4adcf57fe19e6d938e2142fbf3cfd
JoyBoy102 <155880623+JoyBoy102@users.noreply.github.com>
2024-06-16 15:32:24 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.Location = new Point(121, 41);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(644, 399);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(154, 520);
+            checkBox1.Margin = new Padding(3, 4, 3, 4);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(340, 24);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Выделение двух кругов при их пересечении";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(521, 520);
+            checkBox2.Margin = new Padding(3, 4, 3, 4);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(312, 24);
+            checkBox2.TabIndex = 2;
+            checkBox2.Text = "Включить выделение нескольких кругов";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(945, 680);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms.VisualStyles;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public CirclesContainer container = new CirclesContainer();
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            container.checkCursorAll(e.X, e.Y, checkBox1.Checked, checkBox2.Checked);
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            container.paintAllCircles(e.Graphics);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActiveControl = checkBox2;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                container.delete_objects();
+                pictureBox1.Invalidate();
+            }
+            else if (e.KeyCode == Keys.ControlKey)
+            {
+                checkBox2.Checked = true;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                checkBox2.Checked = false;
+            }
+        }
+    }
+
+
+    public class Strategy
+    {
+        public virtual void push_back(ref CCircle[] arr, ref int current_size, CCircle obj) {}
+    }
+    public class StrategyPushBackDefault: Strategy
+    {
+        public override void push_back(ref CCircle[] arr, ref int current_size, CCircle obj) {
+            arr[current_size++] = obj;
+        }
+
+    }
+
+    public class StrategyPushBackLimit: Strategy
+    {
+        public override void push_back(ref CCircle[] arr, ref int current_size, CCircle obj)
+        {
+            CCircle[] new_container = new CCircle[current_size + 10];
+            for (int i = 0; i < current_size; i++)
+                new_container[i] = arr[i];
+
+            new_container[current_size++] = obj;
+            arr = new_container;
+        }
+    }
+
+    public class CCircle
+    {
+        private int x;
+        private int y;
+        private int radius;
+        private bool isSelected;
+        
+        public CCircle(int x, int y, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.isSelected = false;
+        }
+
+        public void paintCircle(Graphics g)
+        {
+            Rectangle rect = new Rectangle(x - radius, y - radius, 2 * radius, 2 * radius);
+            if (isSelected)
+                g.FillEllipse(Brushes.Blue, rect);
+            else
+                g.FillEllipse(Brushes.Red, rect);
+        }
+
+        
+        public bool checkCursor(int mouseX, int mouseY)
+        {
+            int dx = mouseX - x;
+            int dy = mouseY - y;
+            return dx * dx + dy * dy <= radius * radius;
+        }
+
+        public void select()
+        {
+            isSelected = true;
+        }
+
+        public void unselect()
+        {
+            isSelected=false;
+        }
+
+        public bool selected()
+        {
+            return isSelected;
+        }
+    }
+
+
+
+    public class CirclesContainer
+    {
+        private int capacity;
+        private CCircle[] container;
+        private int current_size = 0;
+        private Strategy strategy = new StrategyPushBackDefault();
+        
+        public CirclesContainer()
+        {
+            this.capacity = 10;
+            container = new CCircle[this.capacity];
+            
+        }
+
+        public CirclesContainer(int capacity)
+        {
+            this.capacity = capacity;
+            container = new CCircle[this.capacity];
+        }
+
+        public void push_back(CCircle obj)
+        {
+            if (current_size >= capacity)
+            {
+                strategy = new StrategyPushBackLimit();
+                capacity += 10;
+                strategy.push_back(ref container, ref current_size, obj);
+                
+            }
+            else
+                strategy.push_back(ref container, ref current_size, obj);
+        }
+
+        public void paintAllCircles(Graphics g)
+        {
+            for (int i = 0; i < current_size; i++)
+                container[i].paintCircle(g);
+        }
+
+        
+        public void checkCursorAll(int mouseX, int mouseY, bool checkBox1State, bool checkBox2State)
+        {
+            List<int> indexes = cursorOnCircle(mouseX, mouseY);
+            if (indexes.Count == 0)
+            {
+                unselectall();
+                push_back(new CCircle(mouseX, mouseY, 30));
+                container[current_size - 1].select();
+            }
+            else if (indexes.All(i => container[i].selected()) && get_selected_circles_count()>indexes.Count)
+            {
+                for (int i = 0; i < indexes.Count; i++)
+                    container[indexes[i]].unselect();
+            }
+            
+            else if (!checkBox1State && !checkBox2State)
+            {
+                
+                for (int i = 0; i< indexes.Count; i++)
+                {
+                    unselectall();
+                    container[indexes[i]].select();
+                }
+            }
+            else if (!checkBox1State && checkBox2State)
+            {
+                
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    container[indexes[i]].select();
+                    break;
+                }
+            }
+            else if (checkBox1State && !checkBox2State)
+            {
+                unselectall();
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    container[indexes[i]].select();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    container[indexes[i]].select();
+                }
+            }
+        }
+        
+        public int get_selected_circles_count()
+        {
+            int count = 0;
+            for (int i = 0; i < current_size; i++)
+            {
+                if (container[i].selected())
+                    count++;
+
+            }
+            return count;
+        }
+
+        public void delete_objects()
+        {
+            CCircle[] new_container = new CCircle[capacity];
+            int new_container_index = 0;
+            for (int i = 0; i < current_size; i++)
+            {
+                if (!container[i].selected())
+                {
+                    new_container[new_container_index++] = container[i];
+                }
+            }
+            current_size = new_container_index;
+            if (current_size!=0)
+                new_container[current_size-1].select();
+            container = new_container;
+        }
+
+        public List<int> cursorOnCircle(int mouseX, int mouseY)
+        {
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < current_size; i++)
+            {
+                if (container[i].checkCursor(mouseX, mouseY))
+                    indexes.Add(i);
+            }
+
+            return indexes;
+        }
+
+        public void unselectall()
+        {
+            for (int i = 0; i < current_size; i++)
+            {
+                container[i].unselect();
+                
+            }
+        }
+
+        public int get_size()
+        {
+            return current_size;
+        }
+
+        public CCircle getCircle(int ind)
+        {
+            return container[ind];
+        }
+    }
+
+}
